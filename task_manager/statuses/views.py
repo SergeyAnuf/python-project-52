c2745be57e36acb803c7576283dca55b972b50a2
SergeyAnuf <sergeysyava38@gmail.com>
2025-07-24 19:58:07 +0300

    внесены изменения в перевод

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -40,7 +40,7 @@ class StatusUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = _('Обновить статус')
+        context['title'] = _('Изменить статус')
         return context
 
 
