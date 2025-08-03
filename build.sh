5f459e8fee4466ad50ad5241ee01b902889e1d9f
SergeyAnuf <sergeysyava38@gmail.com>
2025-08-03 15:26:41 +0300

    исправления

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
+# Скачиваем uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
+source $HOME/.cargo/env
 
-make install && make collectstatic && make migrate
+# Установка зависимостей
+uv pip install -r requirements.txt
+
+# Миграции и сборка статики
+python manage.py migrate
+python manage.py collectstatic --noinput
