cb979e0ef5ca817fca006c237bf9954ff6ee5c44
SergeyAnuf <sergeysyava38@gmail.com>
2025-08-24 11:32:14 +0300

    исправление архитектуры проекта

--- conftest.py
+++ conftest.py
@@ -1,6 +1,11 @@
 import os
-import django
-from django.conf import settings
+import sys
+from pathlib import Path
 
+# Добавляем корень проекта в sys.path
+root = Path(__file__).resolve().parent
+sys.path.insert(0, str(root))
+
+import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager.settings')
 django.setup()
