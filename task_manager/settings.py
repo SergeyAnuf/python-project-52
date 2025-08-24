0d4561d564be2af1e7c3e5dae96bdb720b37ebbd
SergeyAnuf <sergeysyava38@gmail.com>
2025-08-24 12:06:14 +0300

    исправление архитектуры проекта

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'task_manager',
     'django_bootstrap5',
-    'task_manager.users.apps.UsersConfig',
+    'task_manager.users',
     'task_manager.statuses',
     'task_manager.tasks',
     'task_manager.labels',
